416c1f30ce00e9f2f253f37c4da274b27f1ecb11
12944qwerty <qwerty12944qwerty@gmail.com>
2021-08-11 15:59:02 -0500

    add separator when dev mode is off
    credit to Joakim

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Plugin } = require('powercord/entities');
 const { getModule, React } = require('powercord/webpack');
 const { inject, uninject } = require("powercord/injector");
+const { findInReactTree } = require('powercord/util');
 
 const { clipboard } = require('electron');
 
@@ -11,16 +12,26 @@ module.exports = class CopyServerIcon extends Plugin {
 
         inject('server-icon-url-button', GuildContextMenu, 'default', 
             ([{ guild }], res) => {
-                res.props.children.push(
-                    React.createElement(Menu.MenuItem, {
-                        id: "icon-url-button",
-                        label: "Copy Icon Url",
-                        action: () => guild.icon ? clipboard.writeText(guild.getIconURL()) : powercord.api.notices.sendToast('no-icon-url', {
-                                               header: `Guild ${guild.name} has no valid Icon.`,
-                                               timeout: 5000
-                                            })
-                    })
-                );
+                const copyIconUrl = React.createElement(Menu.MenuItem, {
+                    id: "icon-url-button",
+                    label: "Copy Icon Url",
+                    action: () => guild.icon ? clipboard.writeText(guild.getIconURL()) : powercord.api.notices.sendToast('no-icon-url', {
+                                        header: `Server '${guild.name}' has no valid Icon.`,
+                                        timeout: 5000
+                                        })
+                });
+
+                const devmodeItem = findInReactTree(res.props.children, child => child.props && child.props.id === 'devmode-copy-id');
+                const developerGroup = res.props.children.find(child => child.props && child.props.children === devmodeItem);
+                if (developerGroup) {
+                    if (!Array.isArray(developerGroup.props.children)) {
+                        developerGroup.props.children = [ developerGroup.props.children ];
+                    }
+            
+                    developerGroup.props.children.push(copyIconUrl);
+                } else {
+                    res.props.children.push([ React.createElement(Menu.MenuSeparator), React.createElement(Menu.MenuGroup, {}, copyIconUrl) ]);
+                }
                 return res;
             }
         );
